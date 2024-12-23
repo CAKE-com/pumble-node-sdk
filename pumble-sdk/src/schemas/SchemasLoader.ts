70fa3bff7a79981894bf1e45cf4c9e453514bf27
xhixha-m <muharrem.xhixha@pumble.com>
2024-12-23 10:06:10 +0100

    Update client validation

--- pumble-sdk/src/schemas/SchemasLoader.ts
+++ pumble-sdk/src/schemas/SchemasLoader.ts
@@ -1,6 +1,5 @@
 import Ajv from 'ajv';
 import { mainBlocks } from './mainBlocks';
-import {attachmentActionsBlock} from "./attachmentActionsBlock";
 
 class SchemasLoader {
     private ajv = new Ajv({
@@ -19,18 +18,6 @@ class SchemasLoader {
         return true;
     }
 
-    public async attachmentBlocksValid(data: any[]): Promise<boolean> {
-        const validate = this.ajv.compile(attachmentActionsBlock);
-        if (validate) {
-            for (let i = 0; i < data.length; ++i) {
-                if (data[i].actions && !validate(data[i].actions)) {
-                    console.error(validate.errors);
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 }
 
 export const schemasLoader = new SchemasLoader();
